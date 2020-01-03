badc9c26486c3cabe6f03c75ccee78b0bf7f28e5
沉迷JS无法自拔 <243253277@qq.com>
2020-01-03 08:13:25 +0800

    Add files via upload

---
+++ 24-反射光实现/script.js
@@ -0,0 +1,309 @@
+// sample_011
+//
+// 使用WebGL的漫射光（定向光）+环境光（环境光）+反射光（镜面光）
+
+onload = function () {
+    // 获取画布元素
+    var c = document.getElementById('canvas');
+    c.width = 500;
+    c.height = 300;
+
+    // 获取webgl上下文
+    var gl = c.getContext('webgl') || c.getContext('experimental-webgl');
+
+    // 顶点和片段着色器生成
+    var v_shader = create_shader('vs');
+    var f_shader = create_shader('fs');
+
+    // 创建和链接程序对象
+    var prg = create_program(v_shader, f_shader);
+
+    // 在数组中获取attributeLocation
+    var attLocation = new Array();
+    attLocation[0] = gl.getAttribLocation(prg, 'position');
+    attLocation[1] = gl.getAttribLocation(prg, 'normal');
+    attLocation[2] = gl.getAttribLocation(prg, 'color');
+
+    // 将属性的元素数存储在数组中
+    var attStride = new Array();
+    attStride[0] = 3;
+    attStride[1] = 3;
+    attStride[2] = 4;
+
+    // 生成圆环顶点数据
+    var torusData = torus(32, 32, 1.0, 2.0);
+    var position = torusData[0];
+    var normal = torusData[1];
+    var color = torusData[2];
+    var index = torusData[3];
+
+    // 生成VBO
+    var pos_vbo = create_vbo(position);
+    var nor_vbo = create_vbo(normal);
+    var col_vbo = create_vbo(color);
+
+    // 注册VBO
+    set_attribute([pos_vbo, nor_vbo, col_vbo], attLocation, attStride);
+
+    // IBO生成
+    var ibo = create_ibo(index);
+
+    // 绑定并注册IBO
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+
+    // 在数组中获取uniformLocation
+    var uniLocation = new Array();
+    uniLocation[0] = gl.getUniformLocation(prg, 'mvpMatrix');
+    uniLocation[1] = gl.getUniformLocation(prg, 'invMatrix');
+    uniLocation[2] = gl.getUniformLocation(prg, 'lightDirection');
+    uniLocation[3] = gl.getUniformLocation(prg, 'eyeDirection');
+    uniLocation[4] = gl.getUniformLocation(prg, 'ambientColor');
+
+    //使用minMatrix.js进行与矩阵相关的处理
+    //创建matIV对象
+    var m = new matIV();
+
+    // 各种矩阵的生成和初始化
+    var mMatrix = m.identity(m.create());
+    var vMatrix = m.identity(m.create());
+    var pMatrix = m.identity(m.create());
+    var tmpMatrix = m.identity(m.create());
+    var mvpMatrix = m.identity(m.create());
+    var invMatrix = m.identity(m.create());
+
+    // 查看x投影坐标变换矩阵
+    m.lookAt([0.0, 0.0, 20.0], [0, 0, 0], [0, 1, 0], vMatrix);
+    m.perspective(45, c.width / c.height, 0.1, 100, pMatrix);
+    m.multiply(pMatrix, vMatrix, tmpMatrix);
+
+    // 平行光源的方向
+    var lightDirection = [-0.5, 0.5, 0.5];
+
+    // 透视矢量
+    var eyeDirection = [0.0, 0.0, 20.0];
+
+    // 环境光色
+    var ambientColor = [0.1, 0.1, 0.1, 1.0];
+
+    // 宣布柜台
+    var count = 0;
+
+    // 启用剔除和深度测试
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.enable(gl.CULL_FACE);
+
+    // 恒定循环
+    (function () {
+        // 初始化画布
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearDepth(1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        // カウンタをインクリメントする
+        count++;
+
+        // カウンタを元にラジアンを算出
+        var rad = (count % 360) * Math.PI / 180;
+
+        // モデル座標変換行列の生成
+        m.identity(mMatrix);
+        m.rotate(mMatrix, rad, [0, 1, 1], mMatrix);
+        m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+
+        // モデル座標変換行列から逆行列を生成
+        m.inverse(mMatrix, invMatrix);
+
+        // uniform変数の登録
+        gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
+        gl.uniformMatrix4fv(uniLocation[1], false, invMatrix);
+        gl.uniform3fv(uniLocation[2], lightDirection);
+        gl.uniform3fv(uniLocation[3], eyeDirection);
+        gl.uniform4fv(uniLocation[4], ambientColor);
+
+        // モデルの描画
+        gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
+
+        // コンテキストの再描画
+        gl.flush();
+
+        // ループのために再帰呼び出し
+        setTimeout(arguments.callee, 1000 / 30);
+    })();
+
+    // シェーダを生成する関数
+    function create_shader(id) {
+        // シェーダを格納する変数
+        var shader;
+
+        // HTMLからscriptタグへの参照を取得
+        var scriptElement = document.getElementById(id);
+
+        // scriptタグが存在しない場合は抜ける
+        if (!scriptElement) { return; }
+
+        // scriptタグのtype属性をチェック
+        switch (scriptElement.type) {
+
+            // 頂点シェーダの場合
+            case 'x-shader/x-vertex':
+                shader = gl.createShader(gl.VERTEX_SHADER);
+                break;
+
+            // フラグメントシェーダの場合
+            case 'x-shader/x-fragment':
+                shader = gl.createShader(gl.FRAGMENT_SHADER);
+                break;
+            default:
+                return;
+        }
+
+        // 生成されたシェーダにソースを割り当てる
+        gl.shaderSource(shader, scriptElement.text);
+
+        // シェーダをコンパイルする
+        gl.compileShader(shader);
+
+        // シェーダが正しくコンパイルされたかチェック
+        if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+
+            // 成功していたらシェーダを返して終了
+            return shader;
+        } else {
+
+            // 失敗していたらエラーログをアラートする
+            alert(gl.getShaderInfoLog(shader));
+        }
+    }
+
+    // プログラムオブジェクトを生成しシェーダをリンクする関数
+    function create_program(vs, fs) {
+        // プログラムオブジェクトの生成
+        var program = gl.createProgram();
+
+        // プログラムオブジェクトにシェーダを割り当てる
+        gl.attachShader(program, vs);
+        gl.attachShader(program, fs);
+
+        // シェーダをリンク
+        gl.linkProgram(program);
+
+        // シェーダのリンクが正しく行なわれたかチェック
+        if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+
+            // 成功していたらプログラムオブジェクトを有効にする
+            gl.useProgram(program);
+
+            // プログラムオブジェクトを返して終了
+            return program;
+        } else {
+
+            // 失敗していたらエラーログをアラートする
+            alert(gl.getProgramInfoLog(program));
+        }
+    }
+
+    // VBOを生成する関数
+    function create_vbo(data) {
+        // バッファオブジェクトの生成
+        var vbo = gl.createBuffer();
+
+        // バッファをバインドする
+        gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+
+        // バッファにデータをセット
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+
+        // バッファのバインドを無効化
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+        // 生成した VBO を返して終了
+        return vbo;
+    }
+
+    // VBOをバインドし登録する関数
+    function set_attribute(vbo, attL, attS) {
+        // 引数として受け取った配列を処理する
+        for (var i in vbo) {
+            // バッファをバインドする
+            gl.bindBuffer(gl.ARRAY_BUFFER, vbo[i]);
+
+            // attributeLocationを有効にする
+            gl.enableVertexAttribArray(attL[i]);
+
+            // attributeLocationを通知し登録する
+            gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
+        }
+    }
+
+    // IBOを生成する関数
+    function create_ibo(data) {
+        // バッファオブジェクトの生成
+        var ibo = gl.createBuffer();
+
+        // バッファをバインドする
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+
+        // バッファにデータをセット
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
+
+        // バッファのバインドを無効化
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+        // 生成したIBOを返して終了
+        return ibo;
+    }
+
+    // トーラスを生成する関数
+    function torus(row, column, irad, orad) {
+        var pos = new Array(), nor = new Array(),
+            col = new Array(), idx = new Array();
+        for (var i = 0; i <= row; i++) {
+            var r = Math.PI * 2 / row * i;
+            var rr = Math.cos(r);
+            var ry = Math.sin(r);
+            for (var ii = 0; ii <= column; ii++) {
+                var tr = Math.PI * 2 / column * ii;
+                var tx = (rr * irad + orad) * Math.cos(tr);
+                var ty = ry * irad;
+                var tz = (rr * irad + orad) * Math.sin(tr);
+                var rx = rr * Math.cos(tr);
+                var rz = rr * Math.sin(tr);
+                pos.push(tx, ty, tz);
+                nor.push(rx, ry, rz);
+                var tc = hsva(360 / column * ii, 1, 1, 1);
+                col.push(tc[0], tc[1], tc[2], tc[3]);
+            }
+        }
+        for (i = 0; i < row; i++) {
+            for (ii = 0; ii < column; ii++) {
+                r = (column + 1) * i + ii;
+                idx.push(r, r + column + 1, r + 1);
+                idx.push(r + column + 1, r + column + 2, r + 1);
+            }
+        }
+        return [pos, nor, col, idx];
+    }
+
+    // HSVカラー取得用関数
+    function hsva(h, s, v, a) {
+        if (s > 1 || v > 1 || a > 1) { return; }
+        var th = h % 360;
+        var i = Math.floor(th / 60);
+        var f = th / 60 - i;
+        var m = v * (1 - s);
+        var n = v * (1 - s * f);
+        var k = v * (1 - s * (1 - f));
+        var color = new Array();
+        if (!s > 0 && !s < 0) {
+            color.push(v, v, v, a);
+        } else {
+            var r = new Array(v, n, m, m, k, v);
+            var g = new Array(k, v, v, n, m, m);
+            var b = new Array(m, m, k, v, v, n);
+            color.push(r[i], g[i], b[i], a);
+        }
+        return color;
+    }
+
+};
